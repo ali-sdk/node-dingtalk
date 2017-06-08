497d78f30aa26a9a79a3e9845a0c802c51f728ce
taoqiufeng <taoqiufeng@ifeidao.com>
2017-06-08 20:44:13 +0800

    add task `dev`

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,16 @@ gulp.task('compile-ts', () => {
 		.pipe(gulp.dest(dest));
 });
 
+gulp.task('compile-ts-es5', () => {
+  const ts = require('gulp-typescript');
+  const tsProject = ts.createProject('./tsconfig.json');
+  tsProject.options.target = 1;
+  const dest = tsProject.options.outDir;
+  return tsProject.src()
+		.pipe(tsProject())
+		.pipe(gulp.dest(dest));
+});
+
 gulp.task('copy-files', () => {
   return gulp.src([ './package.json', './README.md' ])
 		.pipe(gulp.dest('./dist/'));
@@ -30,4 +40,7 @@ gulp.task('default', cb => {
   return sequence('clean', 'compile-ts', 'copy-files', cb);
 });
 
-gulp.task('dev', [ 'compile-ts-umd', 'watch' ]);
+gulp.task('dev', cb => {
+  const sequence = require('gulp-sequence');
+  return sequence('clean', 'compile-ts-es5', 'copy-files', cb);
+});
