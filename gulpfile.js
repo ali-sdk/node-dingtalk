0c07a46009eeecc1d04d8f40fbbb655f567f9899
taoqiufeng <taoqiufeng@ifeidao.com>
2017-06-08 20:49:02 +0800

    compie ts to es6 in dev task

--- gulpfile.js
+++ gulpfile.js
@@ -2,45 +2,45 @@
 const gulp = require('gulp');
 
 gulp.task('clean', () => {
-  const del = require('del');
-  return del([ './dist/' ]);
+	const del = require('del');
+	return del(['./dist/']);
 });
 
 gulp.task('compile-ts', () => {
-  const ts = require('gulp-typescript');
-  const tsProject = ts.createProject('./tsconfig.json');
-  const dest = tsProject.options.outDir;
-  return tsProject.src()
+	const ts = require('gulp-typescript');
+	const tsProject = ts.createProject('./tsconfig.json');
+	const dest = tsProject.options.outDir;
+	return tsProject.src()
 		.pipe(tsProject())
 		.pipe(gulp.dest(dest));
 });
 
-gulp.task('compile-ts-es5', () => {
-  const ts = require('gulp-typescript');
-  const tsProject = ts.createProject('./tsconfig.json');
-  tsProject.options.target = 1;
-  const dest = tsProject.options.outDir;
-  return tsProject.src()
+gulp.task('compile-ts-es6', () => {
+	const ts = require('gulp-typescript');
+	const tsProject = ts.createProject('./tsconfig.json');
+	tsProject.options.target = 2;
+	const dest = tsProject.options.outDir;
+	return tsProject.src()
 		.pipe(tsProject())
 		.pipe(gulp.dest(dest));
 });
 
 gulp.task('copy-files', () => {
-  return gulp.src([ './package.json', './README.md' ])
+	return gulp.src(['./package.json', './README.md'])
 		.pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('watch', () => {
-  return gulp.start([ 'watch-ts' ]);
+	return gulp.start(['watch-ts']);
 });
 
 // =======================================================================
 gulp.task('default', cb => {
-  const sequence = require('gulp-sequence');
-  return sequence('clean', 'compile-ts', 'copy-files', cb);
+	const sequence = require('gulp-sequence');
+	return sequence('clean', 'compile-ts', 'copy-files', cb);
 });
 
 gulp.task('dev', cb => {
-  const sequence = require('gulp-sequence');
-  return sequence('clean', 'compile-ts-es5', 'copy-files', cb);
+	const sequence = require('gulp-sequence');
+	return sequence('clean', 'compile-ts-es6', 'copy-files', cb);
 });
