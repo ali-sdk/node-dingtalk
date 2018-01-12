c9f19a5203c5b5b460dcb3fdf735b2033da700c4
fengmk2 <fengmk2@gmail.com>
2018-01-12 17:04:23 +0800

    f

--- test/lib/api/multi.test.js
+++ test/lib/api/multi.test.js
@@ -7,15 +7,15 @@ const options = require('./../../fixtures/test.config');
 
 describe('test/lib/api/multi.test.js', () => {
   let dingtalk;
-  const corpid1 = process.env.NODE_DINGTALK_TEST_CORPID || 'NODE_DINGTALK_TEST_CORPID';
-  const corpsecret1 = process.env.NODE_DINGTALK_TEST_CORPSECRET || 'NODE_DINGTALK_TEST_CORPSECRET';
-  const appid1 = process.env.NODE_DINGTALK_TEST_APPID || 'NODE_DINGTALK_TEST_APPID';
-  const appsecret1 = process.env.NODE_DINGTALK_TEST_APPSECRET || 'NODE_DINGTALK_TEST_APPSECRET';
+  const corpid1 = process.env.NODE_DINGTALK_TEST_CORPID || options.corpid;
+  const corpsecret1 = process.env.NODE_DINGTALK_TEST_CORPSECRET || options.corpsecret;
+  const appid1 = process.env.NODE_DINGTALK_TEST_APPID || options.appid;
+  const appsecret1 = process.env.NODE_DINGTALK_TEST_APPSECRET || options.appsecret;
 
-  const corpid2 = process.env.NODE_DINGTALK_TEST_CORPID2 || 'NODE_DINGTALK_TEST_CORPID2';
-  const corpsecret2 = process.env.NODE_DINGTALK_TEST_CORPSECRET2 || 'NODE_DINGTALK_TEST_CORPSECRET2';
-  const appid2 = process.env.NODE_DINGTALK_TEST_APPID2 || 'NODE_DINGTALK_TEST_APPID2';
-  const appsecret2 = process.env.NODE_DINGTALK_TEST_APPSECRET2 || 'NODE_DINGTALK_TEST_APPSECRET2';
+  const corpid2 = process.env.NODE_DINGTALK_TEST_CORPID2;
+  const corpsecret2 = process.env.NODE_DINGTALK_TEST_CORPSECRET2;
+  const appid2 = process.env.NODE_DINGTALK_TEST_APPID2;
+  const appsecret2 = process.env.NODE_DINGTALK_TEST_APPSECRET2;
 
   before(function* () {
     dingtalk = new DingTalk(options);
@@ -35,11 +35,13 @@ describe('test/lib/api/multi.test.js', () => {
     assert(token === token2);
 
     // other corp
-    const token3 = yield dingtalk.client.getAccessToken({
-      corpid: corpid2,
-      corpsecret: corpsecret2,
-    });
-    assert(token !== token3);
+    if (corpid2) {
+      const token3 = yield dingtalk.client.getAccessToken({
+        corpid: corpid2,
+        corpsecret: corpsecret2,
+      });
+      assert(token !== token3);
+    }
   });
 
   it('getAccessToken from app', function* () {
@@ -56,11 +58,13 @@ describe('test/lib/api/multi.test.js', () => {
     assert(token === token2);
 
     // other corp
-    const token3 = yield dingtalk.client.getAccessToken({
-      appid: appid2,
-      appsecret: appsecret2,
-    });
-    assert(token !== token3);
+    if (appid2) {
+      const token3 = yield dingtalk.client.getAccessToken({
+        appid: appid2,
+        appsecret: appsecret2,
+      });
+      assert(token !== token3);
+    }
   });
 
   it('getQRConnectUrl', function* () {
@@ -75,11 +79,13 @@ describe('test/lib/api/multi.test.js', () => {
     });
     assert(url === url1);
 
-    const url2 = yield dingtalk.client.getQRConnectUrl({
-      redirect_uri: 'http://127.0.0.1:7001/auth/callback/dingding',
-      appid: appid2,
-    });
-    assert(url2.includes(appid2));
+    if (appid2) {
+      const url2 = yield dingtalk.client.getQRConnectUrl({
+        redirect_uri: 'http://127.0.0.1:7001/auth/callback/dingding',
+        appid: appid2,
+      });
+      assert(url2.includes(appid2));
+    }
   });
 
   it('getIframeQRGotoUrl', function* () {
@@ -94,10 +100,12 @@ describe('test/lib/api/multi.test.js', () => {
     });
     assert(url === url1);
 
-    const url2 = yield dingtalk.client.getIframeQRGotoUrl({
-      redirect_uri: 'http://127.0.0.1:7001/auth/callback/dingding',
-      appid: appid2,
-    });
-    assert(url2.includes(appid2));
+    if (appid2) {
+      const url2 = yield dingtalk.client.getIframeQRGotoUrl({
+        redirect_uri: 'http://127.0.0.1:7001/auth/callback/dingding',
+        appid: appid2,
+      });
+      assert(url2.includes(appid2));
+    }
   });
 });
